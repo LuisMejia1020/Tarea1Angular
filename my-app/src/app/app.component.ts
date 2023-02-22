a01809cd45942cc66ef5c85091f40292fa9b5d1f
Luis Mejia <luisrvp5@gmail.com>
2023-02-21 20:59:18 -0600

    trabajo de angular

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { IEnlace } from './interfaces/IEnlace';
+import { IMangas } from './interfaces/IEnlace';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-app';
+  title = 'MangasJaponeses';
+  descripcion = 'Esta es una aplicacion para leer mangas en linea de Japon'
+  estrenos = 'cada manga se estrena semanalmente'
+  noticias = 'Tower of god regresa'
+  colores: string[] = []
+  enlace: IEnlace = {
+    msg: 'bienvenido',
+    enlace:'https://www.google.com'
+  }
+
+  mangas: IMangas[] = [
+    {nombre: 'Tower of God',
+    nombreCreador: 'SIU',
+    emision: true,
+    animacion: true,
+    descripcion: 'Subir la torre para convertirse en alguien especial '},
+
+    {nombre: 'Naruto',
+    nombreCreador: 'Kishimoto',
+    emision: true,
+    animacion: true,
+    descripcion: 'trabaja para ser un ninja'},
+
+    {nombre: 'Jujutsu Kaisen',
+    nombreCreador: 'Gege Akutami',
+    emision: true,
+    animacion: true,
+    descripcion: 'vuelvete el mas poderoso '},
+
+  ]
+
+  
+  
+
+  constructor(){
+    this.colores.push('rojo')
+    this.colores.push('verde')
+    this.colores.push('blanco')
+  }
+
 }
+
+
---
+++ my-app/src/app/interfaces/IEnlace.ts
@@ -0,0 +1,15 @@
+export interface IEnlace {
+    msg: string,
+    enlace: string 
+  }
+
+  export interface IMangas {
+    nombre: string,
+    nombreCreador: string,
+    emision: boolean,
+    animacion: boolean,
+    descripcion: string
+
+  }
+
+  
